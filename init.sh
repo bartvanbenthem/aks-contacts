c652351f943201d0c99934adf452b0cb33411cc7
Bart van benthem <bart.vanbenthem@ns.nl>
2020-08-27 18:29:15 +0200

    updated repo and module name

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # initialize go modules
-go mod init github.com/bartvanbenthem/aks2contact
+go mod init github.com/bartvanbenthem/aks-contacts
 # get the correct go-client module
 go get k8s.io/client-go@kubernetes-1.16.7
 
